8a34dd9f9df1d1f0c0545b235be9e2c52ea6e5e5
pavlonion <pavlonion@gmail.com>
2017-12-27 23:55:07 +0300

    motion

--- main.go
+++ main.go
@@ -3,48 +3,112 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"strconv"
+// 	"strconv"
 	"log"
 	"text/template"
 	"math/rand"
+	"container/list"
 )
 
 const viewSize = 16
-var alive = true
-var hero = "&Omega;"
-var cross = "&#x2626;"
 const water = "~"
 const ground = "."
+
+type Hero struct {
+	Alive bool
+	X int64
+	Y int64
+}
+
+func NewHero() (Hero) {
+	hero := Hero{}
+	hero.Init();
+	return hero
+}
+
+func (hero Hero) Init() {
+	hero.Alive = true
+	hero.X = 0
+	hero.Y = 0	
+}
+
+func (hero Hero) Symbol() (string) {
+	if hero.Alive {
+		return "&Omega;"
+
+	} else {
+		return "&#x2626;"
+	}
+}
+
+var hero = NewHero()
+
 var terrains = [2]string {"~", "."}
 var view [][]string
-var world [][]string
+var viewLandscape [][]string
+
+var world_latitude = list.New()
+var world_longitude = list.New()
 
 func handlerRoot(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
-	fmt.Println("path", r.URL.Path)
 
 	if r.URL.Path != "/" {
 		handlerError(w, r, http.StatusNotFound)
 		return
 	}
 
-	t, _ := template.ParseFiles("index.html")
-	x, _ := strconv.Atoi(r.Form.Get("x"))
-	y, _ := strconv.Atoi(r.Form.Get("y"))
+// 	hero.X, _ = strconv.ParseInt(r.Form.Get("x"), 10, 64)
+// 	hero.Y, _ = strconv.ParseInt(r.Form.Get("y"), 10, 64)
 
-	if !alive {
-		fmt.Println("redirect")
-		http.Redirect(w, r, "/", 302)
-		alive = true;
+	executeTemplate(w, r, hero)
+}
+
+func handlerMove(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+
+	if r.URL.Path != "/move/" {
+		handlerError(w, r, http.StatusNotFound)
 		return
 	}
 
-	alive = placeHero(x, y)
-	fmt.Println("alive", alive)
+	direction := r.Form.Get("direction")
 
-	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	t.Execute(w, struct{View [][]string}{view})
+	if direction == "left" {
+		hero.X--
+	} else if direction == "right" {
+		hero.X++
+	} else if direction == "up" {
+		hero.Y--
+	} else if direction == "down" {
+		hero.Y++
+	}
+
+	executeTemplate(w, r, hero)
 }
+
+func executeTemplate(w http.ResponseWriter, r *http.Request, hero Hero) {
+	if !hero.Alive {
+		hero.Init();
+// 		fmt.Println("redirect to /", hero)
+// 		http.Redirect(w, r, "/", 302)
+// 		return
+	}
+
+	placeHero(hero)
+	fmt.Println("hero.Alive", hero.Alive)
+	
+	w.Header().Set("Content-Type", "text/html; charset=utf-8")
+	t, err := template.ParseFiles("index.html")
+	
+	if err != nil {
+		handlerError(w, r, http.StatusInternalServerError)
+		return
+	}
+	
+	t.Execute(w, struct{View [][]string}{view})	
+}
+
 
 func handlerFavicon(w http.ResponseWriter, r *http.Request) {
 	handlerError(w, r, http.StatusNotFound)
@@ -52,19 +116,22 @@ func handlerFavicon(w http.ResponseWriter, r *http.Request) {
 
 func handlerError(w http.ResponseWriter, r *http.Request, status int) {
 	w.WriteHeader(status)
+
 	if status == http.StatusNotFound {
 		fmt.Fprint(w, "404 Not Found")
+	} else {
+		fmt.Fprint(w, "Error")
 	}
 }
 
-func placeHero(x, y int) (bool) {
+func placeHero(hero Hero) {
 	for i := 0; i < viewSize; i++ {
-		view[i] = make([]string, len(world[i]))
-		copy(view[i], world[i]);
+		view[i] = make([]string, len(viewLandscape[i]))
+		copy(view[i], viewLandscape[i]);
 	}
 
-	x = x%viewSize
-	y = y%viewSize
+	x := hero.X % viewSize
+	y := hero.Y % viewSize
 
 	if x < 0 {
 		x = viewSize + x
@@ -77,29 +144,42 @@ func placeHero(x, y int) (bool) {
 	place := &view[y][x]
 
 	if *place == water {
-		*place = cross
-		return false
+		hero.Alive = false
+	} else {
+		hero.Alive = true
 	}
 
-	*place = hero
-	return true
+	*place = hero.Symbol()
+}
+
+func generateLandscape() ([][]string) {
+	var landscape [][]string
+
+	for i := 0; i < viewSize; i++ {
+		landscape = append(landscape, []string{})
+		
+		for j := 0; j < viewSize; j++ {
+			landscape[i] = append(landscape[i], terrains[rand.Intn(len(terrains))])
+		}
+	}
+	
+	return landscape
 }
 
 func main() {
-	for i := 0; i < viewSize; i++ {
-		world = append(world, []string{})
+	viewLandscape = generateLandscape()
+	viewLandscape[0][0] = ground
+	view = make([][]string, len(viewLandscape))
 
-		for j := 0; j < viewSize; j++ {
-			world[i] = append(world[i], terrains[rand.Intn(len(terrains))])
-		}
-	}
+	world_latitude.PushBack(viewLandscape)
+	world_longitude.PushBack(viewLandscape)
 
-	world[0][0] = ground
-
-	view = make([][]string, len(world))
 	http.HandleFunc("/", handlerRoot)
+	http.HandleFunc("/move/", handlerMove)
 	http.HandleFunc("/favicon.ico", handlerFavicon)
+
 	err := http.ListenAndServe(":8080", nil)
+
 	if err != nil {
 		log.Fatal("ListenAndServe: ", err)
 	}
